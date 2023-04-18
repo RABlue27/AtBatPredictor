a03b5e537ccb84bdc8efceec59afade8acdcbac6
Alex <32850744+RABlue27@users.noreply.github.com>
2023-04-18 17:21:24 -0400

    Update seasonStats.py

--- seasonStats.py
+++ seasonStats.py
@@ -3,8 +3,12 @@ from pybaseball import schedule_and_record
 import pandas as pd
 
 
+
+# Statcast data only goes back to 2015. Remove the advanced (launch_speed, launch_angle, spin_rate, etc if you want to train longer)
+# https://pypi.org/project/pybaseball/
+
 sc = statcast(start_dt="2015-01-01", end_dt="2023-04-16")
-sc = sc.loc[:, ["pitch_type", "game_date", "batter", "pitcher", "events", "stand",
+sc = sc.loc[:, ["pitch_type", "game_date", "batter", "pitcher", "events", "stand", "inning", "pitch_number", 
                 "p_throws", "home_team", "away_team", "hit_distance_sc", "launch_speed",
                 "launch_angle"]]
 sc = sc[sc["events"].notnull()]
